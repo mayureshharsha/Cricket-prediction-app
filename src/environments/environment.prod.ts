409161f050d9d12755f99fa566ea94f07fdd70eb
mayuresh <hrlive123@gmail.com>
2019-05-29 11:03:24 +0530

    minor changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   hostUrl: '',
-  matchDataUrl: ''
+  matchurl: 'https://raw.githubusercontent.com/aniket7777777/cricpred/branch/testing/Data_Matches.json'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 
 export const environment = {
   production: false,
-  hostUrl: 'http://192.168.0.108:8090',
+  hostUrl: ':8090',
   matchurl: 'https://raw.githubusercontent.com/aniket7777777/cricpred/branch/testing/Data_Matches.json'
 };
 
