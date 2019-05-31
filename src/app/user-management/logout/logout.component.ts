86d81d3dd1ebbacd38e19b6dc8f65a08bebda81a
mayureshharsha <hrlive123@gmail.com>
2019-05-31 17:19:56 +0530

    Update logout.component.ts

--- src/app/user-management/logout/logout.component.ts
+++ src/app/user-management/logout/logout.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {Router} from "@angular/router";
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-logout',
@@ -12,10 +12,20 @@ export class LogoutComponent implements OnInit {
   }
 
   ngOnInit() {
-    document.cookie = ""
+    this.deleteAllCookies();
 
+    this.router.navigateByUrl('/login');
+  }
 
-    this.router.navigateByUrl('/login')
+  deleteAllCookies() {
+    const cookies = document.cookie.split(';');
+
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i];
+      const eqPos = cookie.indexOf('=');
+      const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+      document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
+    }
   }
 
 }
