af068f96c08c1d600834a70ba74670028bc5b5c1
mayureshharsha <hrlive123@gmail.com>
2019-05-31 17:18:56 +0530

    Update login.component.ts

--- src/app/user-management/login/login.component.ts
+++ src/app/user-management/login/login.component.ts
@@ -26,7 +26,8 @@ export class LoginComponent implements OnInit {
   login() {
     this.loginService.login(this.user).subscribe(
       (response) => {
-        document.cookie = JSON.stringify(response)
+        this.deleteAllCookies();
+        document.cookie = JSON.stringify(response);
         this.router.navigateByUrl('/home');
       },
       error => {
@@ -36,6 +37,18 @@ export class LoginComponent implements OnInit {
       }
     );
 
+  }
+
+
+  deleteAllCookies() {
+    const cookies = document.cookie.split(';');
+
+    for (let i = 0; i < cookies.length; i++) {
+      const cookie = cookies[i];
+      const eqPos = cookie.indexOf('=');
+      const name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+      document.cookie = name + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT';
+    }
   }
 
   register() {
