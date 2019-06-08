ef10157eb5614ef07e584b4dc15f96fd200bf44f
aniket7777777 <aniket_777@hotmail.cim>
2019-06-09 01:17:05 +0530

    timer fixed bg image fixed opacity added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  hostUrl: 'http://ec2-3-17-133-150.us-east-2.compute.amazonaws.com:8090',
+  hostUrl: '',
   matchurl: 'https://raw.githubusercontent.com/aniket7777777/cricpred/master/Data_Matches.json'
 };
